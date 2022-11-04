270eb7a2231bc3376975a6ef891bb3c8bbe1a401
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-11-04 20:13:12 +0530

    first2

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -126,7 +126,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
