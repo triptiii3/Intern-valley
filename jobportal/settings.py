f2b6309cc1147d6c5c622eebe36da024519dc241
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-11-04 20:23:21 +0530

    first3

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
@@ -133,7 +133,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_ROOT = (
-    os.path.join(BASE_DIR,'static')
-)
+
 
